21689f82ca8d3072f97702f2e27dfb46dad930b5
JuanIJX <srwe-@hotmail.com>
2023-05-06 15:03:13 +0200

    Reestructura general de los prototypes

--- src/libraries/utils.mjs
+++ src/libraries/utils.mjs
@@ -1,5 +1,5 @@
 /**
- * Last modified: 05/05/2023
+ * Last modified: 06/05/2023
  */
 
 import url from "url"
@@ -11,7 +11,7 @@ import { exec } from "child_process";
 // Private functions
 
 function getZero(n = 0) {
-	let cad = '0';
+	let cad = '';
 	for (let i = 0; i < n; i++)
 		cad += '0';
 	return cad;
@@ -22,188 +22,194 @@ function getZero(n = 0) {
 // PROTOTYPES
 
 // Array
-Array.prototype.filter2 = function(func) {
-	for (let i = 0; i < this.length; i++) {
-		if(func(this[i]) !== true) {
-			this.splice(i, 1);
-			i--;
+Object.defineProperties(Array.prototype, {
+	"filter2": { value: function(func) {
+		for (let i = 0; i < this.length; i++) {
+			if(func(this[i]) !== true)
+				this.splice(i--, 1);
 		}
-	}
-	return this;
-};
-Array.prototype.deleteElement = function(element) {
-	var pos = this.indexOf(element);
-	if(pos !== -1)
-		this.splice(pos, 1);
-	return this;
-}
-Array.prototype.unique = function() {
-	var a = this.concat();
-	for(var i=0; i<a.length; ++i) {
-		for(var j=i+1; j<a.length; ++j) {
-			if(a[i] === a[j])
-				a.splice(j--, 1);
+		return this;
+	} },
+	"deleteElement": { value: function(element) {
+		var pos = this.indexOf(element);
+		if(pos !== -1)
+			this.splice(pos, 1);
+		return this;
+	} },
+	"unique": { value: function() {
+		var a = this.concat();
+		for(var i=0; i<a.length; ++i) {
+			for(var j=i+1; j<a.length; ++j) {
+				if(a[i] === a[j])
+					a.splice(j--, 1);
+			}
 		}
-	}
-	return a;
-};
-Array.prototype.concat2 = function(...arrays) {
-	for (let index = 0; index < arrays.length; index++) {
-		for (let i = 0; i < arrays[index].length; i++) {
-			if(!this.includes(arrays[index][i]))
-				this.push(arrays[index][i]);
+		return a;
+	} },
+	"concat2": { value: function(...arrays) {
+		for (let index = 0; index < arrays.length; index++) {
+			for (let i = 0; i < arrays[index].length; i++) {
+				if(!this.includes(arrays[index][i]))
+					this.push(arrays[index][i]);
+			}
 		}
-	}
-}
+	} },
+});
 
 // Object
-Object.prototype.getKeyByValue = function(value) {
-	return Object.keys(this).find(key => this[key] === value);
-}
-Object.prototype.clone = function() {
-	return JSON.parse(JSON.stringify(this));
-}
-Object.prototype.assign = function(obj) {
-	for (const key in obj) {
-		if (!Object.hasOwnProperty.call(obj, key)) continue;
-		if (Object.hasOwnProperty.call(this, key)) {
-			const oldElement = this[key];
-			if(Array.isArray(oldElement) && Array.isArray(obj[key]))
-				this[key].concat2(obj[key]);
-			else if(typeof(oldElement)=="object" && oldElement!=null && !Array.isArray(oldElement) && typeof(obj[key])=="object" && obj[key]!=null && !Array.isArray(obj[key]))
-				this[key].assign(obj[key]);
+Object.prototype.clone = function() { return JSON.parse(JSON.stringify(this)); };
+Object.defineProperties(Object.prototype, {
+	"getKeyByValue": { value: function(value) {
+		return Object.keys(this).find(key => this[key] === value);
+	} },
+	"assign": { value: function(obj) {
+		for (const key in obj) {
+			if (!Object.hasOwnProperty.call(obj, key)) continue;
+			if (Object.hasOwnProperty.call(this, key)) {
+				const oldElement = this[key];
+				if(Array.isArray(oldElement) && Array.isArray(obj[key]))
+					this[key].concat2(obj[key]);
+				else if(typeof(oldElement)=="object" && oldElement!=null && !Array.isArray(oldElement) && typeof(obj[key])=="object" && obj[key]!=null && !Array.isArray(obj[key]))
+					this[key].assign(obj[key]);
+			}
+			else
+				this[key] = obj[key]; // Copiar bien copiao, pero temporalmente lo hacemos asi y ale, deberiamos usar clone
 		}
-		else
-			this[key] = obj[key]; // Copiar bien copiao, pero temporalmente lo hacemos asi y ale
-	}
-	return this;
-}
+		return this;
+	} },
+})
 
 // String
-String.prototype.camelCase = function(sep="_", type=true) {
-	const arry = this.toLowerCase().split(sep);
-	if(type)
-		return arry.shift() + arry.reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1), "");
-	return arry.reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1), "");
-}
-String.prototype.rellenar = function(size, character=' ') {
-	var t = true;
-	var r = this;
-	while(r.length < size) {
-		r = t ? r + character : character + r;
-		t = !t;
-	}
-	return r;
-};
-String.prototype.delExt = function() {
-	const pos = this.lastIndexOf(".");
-	if(pos!=-1)
-		return this.substring(0, pos);
-	return this;
-};
-String.prototype.getExt = function() {
-	const pos = this.lastIndexOf(".");
-	if(pos!=-1)
-		return this.substring(pos+1);
-	return this;
-};
-String.prototype.zeroPad = function(n = 2) { return (getZero(n)+this).slice(-1 * (n < this.length ? this.length : n)); };
-String.prototype.suspensivos = function(max, chars="...") { return this.length > max ? this.substring(0, max+chars.length)+chars : this; }
+Object.defineProperties(String.prototype, {
+	"camelCase": { value: function(sep="_", type=true) {
+		const arry = this.toLowerCase().split(sep);
+		if(type)
+			return arry.shift() + arry.reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1), "");
+		return arry.reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1), "");
+	} },
+	"rellenar": { value: function(size, character=' ') {
+		var t = true;
+		var r = this;
+		while(r.length < size) {
+			r = t ? r + character : character + r;
+			t = !t;
+		}
+		return r;
+	} },
+	"delExt": { value: function() {
+		const pos = this.lastIndexOf(".");
+		if(pos!=-1)
+			return this.substring(0, pos);
+		return this;
+	} },
+	"getExt": { value: function() {
+		const pos = this.lastIndexOf(".");
+		if(pos!=-1)
+			return this.substring(pos+1);
+		return this;
+	} },
+	"zeroPad": { value: function(n = 2) { return (getZero(n)+this).slice(-1 * (n < this.length ? this.length : n)); } },
+	"suspensivos": { value: function(max, chars="...") { return this.length > max ? this.substring(0, max+chars.length)+chars : this; } },
+});
 
 // Number
-Number.prototype.zeroPad = function(n = 2) { return (this+"").zeroPad(n) };
+Object.defineProperties(Number.prototype, {
+	"zeroPad": { value: function(n = 2) { return (this+"").zeroPad(n) } }
+});
 
 // Date
-Date.prototype.format= function(format="d/m/Y") {
-	const mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-	const semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-	let cad = "", aux;
-	for (let i = 0; i < format.length; i++) {
-		// https://www.php.net/manual/es/function.date.php
-		switch(format[i]) {
-			// AÑO
-			case "Y": // Una representación numérica completa de un año, 4 dígitos	Ejemplos: 1999 o 2003
-				cad += this.getFullYear();
-				break;
-			case "y": // Una representación de dos dígitos de un año
-				cad += this.getFullYear().zeroPad(2);
-				break;
-
-			// MES
-			case "m": // Representación numérica de un mes, con ceros iniciales	01 hasta 12
-				cad += (this.getMonth() + 1).zeroPad(2);
-				break;
-			case "n": // Representación numérica de un mes, sin ceros iniciales
-				cad += this.getMonth() + 1;
-				break;
-			case "M": // Una representación textual corta de un mes, tres letras
-				cad += mes[this.getMonth()].slice(0, 3);
-				break;
-			case "F": // Una representación textual completa de un mes, como Enero o Marzo
-				cad += mes[this.getMonth()];
-				break;
-
-			// DIA
-			case "j": // Día del mes sin ceros iniciales 1 a 31
-				cad += this.getDate();
-				break;
-			case "d": // Día del mes, 2 dígitos con ceros iniciales	01 a 31
-				cad += this.getDate().zeroPad(2);
-				break;
-			case "D": // Una representación textual de un día, tres letras	Lunes hasta Domingo
-				cad += semana[this.getDay() - 1].slice(0, 3);
-				break;
-
-			// AM / PM
-			case "a": // Ante meridiem y Post meridiem en minúsculas
-				cad += this.getHours() >= 12 ? "pm" : "am";
-				break;
-			case "A": // Ante meridiem y Post meridiem en mayúsculas
-				cad += this.getHours() >= 12 ? "PM" : "AM";
-				break;
-
-			// HORA
-			case "g": // Formato de 12 horas de una hora sin ceros iniciales
-				aux = this.getHours() % 12;
-				cad += aux == 0 ? 12 : aux;
-				break;
-			case "G": // Formato de 24 horas de una hora sin ceros iniciales
-				cad += this.getHours();
-				break;
-			case "h": // Formato de 12 horas de una hora con ceros iniciales
-				aux = this.getHours() % 12;
-				cad += (aux == 0 ? 12 : aux).zeroPad(2);
-				break;
-			case "H": // Formato de 24 horas de una hora con ceros iniciales
-				cad += this.getHours().zeroPad(2);
-				break;
-			
-			// MINUTO
-			case "i": // Minutos con ceros iniciales
-				cad += this.getMinutes().zeroPad(2);
-				break;
-			
-			// SEGUNDO
-			case "s": // Segundos con ceros iniciales
-				cad += this.getSeconds().zeroPad(2);
-				break;
-			
-			// MILISEGUNDO
-			case "v": // Milisegundos
-				cad += this.getMilliseconds();
-				break;
-			case "V": // Milisegundos con 3 ceros iniciales
-				cad += this.getMilliseconds().zeroPad(3);
-				break;
-			
-			// Caracteres
-			default:
-				cad += format[i];
-				break;
+Object.defineProperties(Date.prototype, {
+	"format": { value: function(format="d/m/Y") {
+		const mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+		const semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+		let cad = "", aux;
+		for (let i = 0; i < format.length; i++) {
+			// https://www.php.net/manual/es/function.date.php
+			switch(format[i]) {
+				// AÑO
+				case "Y": // Una representación numérica completa de un año, 4 dígitos	Ejemplos: 1999 o 2003
+					cad += this.getFullYear();
+					break;
+				case "y": // Una representación de dos dígitos de un año
+					cad += this.getFullYear().zeroPad(2);
+					break;
+	
+				// MES
+				case "m": // Representación numérica de un mes, con ceros iniciales	01 hasta 12
+					cad += (this.getMonth() + 1).zeroPad(2);
+					break;
+				case "n": // Representación numérica de un mes, sin ceros iniciales
+					cad += this.getMonth() + 1;
+					break;
+				case "M": // Una representación textual corta de un mes, tres letras
+					cad += mes[this.getMonth()].slice(0, 3);
+					break;
+				case "F": // Una representación textual completa de un mes, como Enero o Marzo
+					cad += mes[this.getMonth()];
+					break;
+	
+				// DIA
+				case "j": // Día del mes sin ceros iniciales 1 a 31
+					cad += this.getDate();
+					break;
+				case "d": // Día del mes, 2 dígitos con ceros iniciales	01 a 31
+					cad += this.getDate().zeroPad(2);
+					break;
+				case "D": // Una representación textual de un día, tres letras	Lunes hasta Domingo
+					cad += semana[this.getDay() - 1].slice(0, 3);
+					break;
+	
+				// AM / PM
+				case "a": // Ante meridiem y Post meridiem en minúsculas
+					cad += this.getHours() >= 12 ? "pm" : "am";
+					break;
+				case "A": // Ante meridiem y Post meridiem en mayúsculas
+					cad += this.getHours() >= 12 ? "PM" : "AM";
+					break;
+	
+				// HORA
+				case "g": // Formato de 12 horas de una hora sin ceros iniciales
+					aux = this.getHours() % 12;
+					cad += aux == 0 ? 12 : aux;
+					break;
+				case "G": // Formato de 24 horas de una hora sin ceros iniciales
+					cad += this.getHours();
+					break;
+				case "h": // Formato de 12 horas de una hora con ceros iniciales
+					aux = this.getHours() % 12;
+					cad += (aux == 0 ? 12 : aux).zeroPad(2);
+					break;
+				case "H": // Formato de 24 horas de una hora con ceros iniciales
+					cad += this.getHours().zeroPad(2);
+					break;
+				
+				// MINUTO
+				case "i": // Minutos con ceros iniciales
+					cad += this.getMinutes().zeroPad(2);
+					break;
+				
+				// SEGUNDO
+				case "s": // Segundos con ceros iniciales
+					cad += this.getSeconds().zeroPad(2);
+					break;
+				
+				// MILISEGUNDO
+				case "v": // Milisegundos
+					cad += this.getMilliseconds();
+					break;
+				case "V": // Milisegundos con 3 ceros iniciales
+					cad += this.getMilliseconds().zeroPad(3);
+					break;
+				
+				// Caracteres
+				default:
+					cad += format[i];
+					break;
+			}
 		}
-	}
-	return cad;
-};
+		return cad;
+	} }
+});
 
 
 

62541e9057bab5e39c318c1d0a6fa0d5af8e45e3
JuanIJX <srwe-@hotmail.com>
2023-05-07 22:22:39 +0200

    Edit CommandManager

--- src/app/manager/commandmanager.js
+++ src/app/manager/commandmanager.js
@@ -1,54 +1,59 @@
 import { Collection, ChannelType } from "discord.js";
+import Module from "../module.js";
+import { wait } from "../../libraries/utils.mjs";
 
 export default class CommandManager {
-	constructor(discordManager) {
-		Object.defineProperty(this, '_discordManager', { value: discordManager });
-		Object.defineProperty(this, '_availableCmds', { value: new Collection() });
+	constructor() {
 		Object.defineProperty(this, '_enabledCmds', { value: new Set() });
+		Object.defineProperty(this, '_availableCmds', { value: new Collection() });
 	}
 
 	enableModule(name) {
-		this._availableCmds.filter(v => v.module.name == name).forEach((v, key) => this._enabledCmds.add(key));
+		this._availableCmds
+			.filter(v => v.module.name == name)
+			.forEach((v, key) => this._enabledCmds.add(key));
 	}
 
 	disableModule(name) {
-		this._availableCmds.filter(v => v.module.name == name).forEach((v, key) => this._enabledCmds.delete(key));
+		this._availableCmds
+			.filter(v => v.module.name == name)
+			.forEach((v, key) => this._enabledCmds.delete(key));
 	}
 
 	disableAll() {
 		this._enabledCmds.clear();
 	}
 
-	addCommand(name, module, data) {
+	addCommand(name, module, action, channels=null) {
 		if(this._availableCmds.has(name))
 			throw new Error(`No se pudo añadir el comando '${name}' porque ya existe`);
-		this._availableCmds.set(name, {
-			module,
-			channels: null,
-			data
-		});
+		if(!(module instanceof Module))
+			throw new Error(`El segundo parámetro debe ser un módulo`);
+		if(typeof(action) != "function")
+			throw new Error(`El tercer parámetro debe ser una función`);
+		if(channels != null && !Array.isArray(channels))
+			throw new Error(`El cuarto parámetro debe ser NULL o un Array`);
+
+		this._availableCmds.set(name, { module, channels, action });
 	}
 
 	commandHandler(message) {
 		if(message.content == "") return;
 		if(message.channel.type == ChannelType.DM || message.channel.type == ChannelType.GroupDM) return;
-		if(message.author.id == this._discordManager.discord.user.id) return;
+		if(message.author.id == message.client.user.id) return;
 
 		const [cmdName, ...args] = message.content.split(" ");
 		if(this._enabledCmds.has(cmdName)) {
-			this._execCmd(this._availableCmds.get(cmdName), [
-				message,
-				cmdName,
-				args.filter2(arg => arg != "")
-			]);
+			const cmdData = this._availableCmds.get(cmdName);
+			if(cmdData.channels == null || cmdData.channels.includes(message.channel.id))
+				this._execCmd(cmdData.action.bind(cmdData.module), [message, cmdName, args.filter2(arg => arg != "")]);
 		}
 	}
 
-	async _execCmd(cmd, params) {
-		if(typeof(cmd.data)=="function") {
-			await cmd.data(...params);
-			/*if(response !== undefined)
-				params[0].channel.send(response);*/
-		}
+	async _execCmd(cmdAction, params) {
+		// Meter al usuario en lista negra para q no spamee el comando, la lista sera por cada guild y por user
+		await cmdAction(...params);
+		// await wait(500); // Tiempo de espera para este usuario
+		// Sacar al usuario de la lista negra
 	}
 }

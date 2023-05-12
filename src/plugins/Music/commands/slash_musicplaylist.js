a5deadbb6d4e7710733bb132e0c5685f35bfa2b0
JuanIJX <srwe-@hotmail.com>
2023-05-12 16:34:28 +0200

    Primera version de slash commands terminada

--- src/plugins/Music/commands/slash_musicplaylist.js
+++ src/plugins/Music/commands/slash_musicplaylist.js
@@ -1,7 +1,4 @@
-import { ApplicationCommandOptionType, ChannelType, SlashCommandBuilder } from "discord.js";
-import { isInteger } from "../../../libraries/utils.mjs";
-import PlaylistManager from "../myplaylist.js";
-
+import { SlashCommandBuilder } from "discord.js";
 export default {
 	slash: new SlashCommandBuilder()
 		.setName('playlist')
@@ -39,6 +36,17 @@ export default {
 			)
 		)
 
+		.addSubcommand(subcommand => subcommand
+			.setName('play')
+			.setDescription('Pone a reproducir tu lista de reproducción')
+			.addIntegerOption(option => option
+				.setName('index')
+				.setDescription('ID o índice de la lista de reproducción')
+				.setMinValue(1)
+				.setRequired(true)
+			)
+		)
+
 		.addSubcommandGroup(group => group
 			.setName('pl')
 			.setDescription('Controla tu lista de reproducción')
@@ -58,15 +66,90 @@ export default {
 					.setMinValue(1)
 				)
 			)
+
+			.addSubcommand(subcommand => subcommand
+				.setName('play')
+				.setDescription('Pone una de las canciones a reproducir')
+				.addIntegerOption(option => option
+					.setName('index')
+					.setDescription('ID o índice de la lista de reproducción')
+					.setMinValue(1)
+					.setRequired(true)
+				)
+				.addIntegerOption(option => option
+					.setName('pos1')
+					.setDescription('Posición de la canción para elegir una específica')
+					.setMinValue(1)
+				)
+				.addIntegerOption(option => option
+					.setName('pos2')
+					.setDescription('Posición de otra canción para elegir varias')
+					.setMinValue(1)
+				)
+			)
+
+			.addSubcommand(subcommand => subcommand
+				.setName('add')
+				.setDescription('Añade una canción a la lista de reproducción')
+				.addIntegerOption(option => option
+					.setName('index')
+					.setDescription('ID o índice de la lista de reproducción')
+					.setMinValue(1)
+					.setRequired(true)
+				)
+				.addStringOption(option => option
+					.setName('song')
+					.setDescription('URL o nombre de la canción que quieres añadir')
+					.setRequired(true)
+				)
+			)
+
+			.addSubcommand(subcommand => subcommand
+				.setName('remove')
+				.setDescription('Elimina una canción de la lista de reproducción')
+				.addIntegerOption(option => option
+					.setName('index')
+					.setDescription('ID o índice de la lista de reproducción')
+					.setMinValue(1)
+					.setRequired(true)
+				)
+				.addIntegerOption(option => option
+					.setName('pos1')
+					.setDescription('Posición de la canción')
+					.setMinValue(1)
+					.setRequired(true)
+				)
+				.addIntegerOption(option => option
+					.setName('pos2')
+					.setDescription('Posición de otra canción para eliminar varias')
+					.setMinValue(1)
+				)
+			)
+
+			.addSubcommand(subcommand => subcommand
+				.setName('clear')
+				.setDescription('Vacía la lista de reproducción')
+				.addIntegerOption(option => option
+					.setName('index')
+					.setDescription('ID o índice de la lista de reproducción')
+					.setMinValue(1)
+					.setRequired(true)
+				)
+			)
 		)
 	,		
 	async execute(interaction) {
 		const user = interaction.user;
+		const channel = interaction.member.voice.channel;
+		const playlistManager = this.musicController.playlistManager;
 		const ops = interaction.options;
 		const cmdName = ops.getSubcommandGroup() ?? ops.getSubcommand(false);
 		
-		let userPlayList = PlaylistManager.get(user);
+		let userPlayList = playlistManager.get(user);
 		let myPlaylist = null;
+		let queue = null;
+
+		let aux_1, aux_2, aux_3, aux_4;
 
 		switch (cmdName) {
 			case "list":
@@ -74,7 +157,7 @@ export default {
 					return "No hay listas de reproducción";
 				return this.getEmbed(userPlayList.embed(), true);
 			case "create":
-				userPlayList ??= PlaylistManager.create(user);
+				userPlayList ??= playlistManager.create(user);
 				userPlayList
 					.create(ops.getString("name"))
 					.setDescription(ops.getString("description") ?? "")
@@ -99,90 +182,91 @@ export default {
 							return `Index máximo: ${userPlayList.size}`
 						myPlaylist = userPlayList.at(ops.getInteger("index")-1);
 
-						return myPlaylist.embed(ops.getInteger("pag")-1);
+						return this.getEmbed(myPlaylist.embed((ops.getInteger("pag") ?? 1) - 1, 20), true);
+					case "add":
+						if(ops.getInteger("index") > userPlayList.size)
+							return `Index máximo: ${userPlayList.size}`
+						myPlaylist = userPlayList.at(ops.getInteger("index")-1);
+
+						aux_2 = await playlistManager.player.search(ops.getString("song"));
+						if (!aux_2.hasTracks())
+							return `No se encuentra cancion alguna`;
+						aux_1 = await myPlaylist.add(aux_2);
+						userPlayList.save();
+
+						if(aux_1.length == 0)
+							return `No se añadió ninguna canción`;
+						if(aux_1.length == 1)
+							return `Se añadió la canción '${aux_1[0].title}'`;
+						return `Se añadieron ${aux_1.length} canciones`;
+					case "remove":
+						if(ops.getInteger("index") > userPlayList.size)
+							return `Index máximo: ${userPlayList.size}`
+						myPlaylist = userPlayList.at(ops.getInteger("index")-1);
+						aux_1 = ops.getInteger("pos1") - 1;
+						aux_2 = (ops.getInteger("pos2") ?? ops.getInteger("pos1")) - 1;
+						if(aux_2 < aux_1) {
+							let aux = aux_1;
+							aux_1 = aux_2;
+							aux_2 = aux;
+						}
+
+						aux_4 = myPlaylist.size;
+						aux_3 = myPlaylist.remove(aux_1, aux_2 + 1 - aux_1);
+						userPlayList.save();
+						switch (aux_3.length) {
+							case 0:
+								return `No se eliminó ninguna canción`
+							case 1:
+								return `Se eliminó la canción [${aux_1+1}] '${aux_3[0].title}'`;
+							default:
+								return `Se eliminaron del ${aux_1+1} al ${aux_2 < aux_4 ? aux_2+1 : aux_4} canciones (${aux_3.length})`;
+						}
+					case "play":
+						if(ops.getInteger("index") > userPlayList.size)
+							return `Index máximo: ${userPlayList.size}`
+						myPlaylist = userPlayList.at(ops.getInteger("index")-1);
+						
+						aux_1 = ops.getInteger("pos1");
+						aux_2 = ops.getInteger("pos2");
+						if(aux_1 == null) {
+							aux_1 = 0;
+							aux_2 = myPlaylist.size - 1;
+						}
+						else if(aux_2 == null) {
+							aux_1--;
+							aux_2 = aux_1;
+						}
+						else {
+							aux_1--;
+							aux_2--;
+							if(aux_2 < aux_1) {
+								let aux = aux_1;
+								aux_1 = aux_2;
+								aux_2 = aux;
+							}
+						}
+
+						if(aux_1 >= myPlaylist.size || aux_2 >= myPlaylist.size)
+							return `Posición de la canción máxima: ${myPlaylist.size}`
+
+						aux_3 = await myPlaylist.getTracks(aux_1, aux_2, user);
+						queue = this.musicController.createQueue(interaction.guild.id);
+						queue.addTrack(aux_3);
+						if(queue.isPlaying)
+							return `Añadido a la cola`;
+						queue.play(channel).then(() => interaction.editReply('Reproduciendo!'));
+						return `Tratando de reproducir...`;
+					case "clear":
+						if(ops.getInteger("index") > userPlayList.size)
+							return `Index máximo: ${userPlayList.size}`
+						userPlayList.at(ops.getInteger("index")-1).clear();
+						userPlayList.save();
+						return `Lista vaciada`;
 				}
 				break;
 		}
 
 		return "default value de la playlist";
 	}
-};
-
-async function x(message, cmdName, args, mainCmdname) {
-	let
-		userPlayList = PlaylistManager.get(message.author),
-		myPlaylist,
-		aux1,
-		aux2;
-
-	switch (args[0]) {
-		case "l":
-		case "list":
-			if(!userPlayList || userPlayList.size == 0)
-				await message.reply("No hay listas de reproducción");
-			else
-				await message.reply(this.getEmbed(userPlayList.embed()));
-			break;
-		case "create":
-			if(args.length <= 1)
-				await message.reply("Falta el nombre de la playlist");
-			else {
-				aux1 = message.content.substr(message.content.indexOf(args[0]) + args[0].length).trim();
-				if(aux1.length > 100)
-					await message.reply("No puede exceder los 100 caracteres");
-				else {
-					userPlayList ??= PlaylistManager.create(message.author);
-					userPlayList.create(aux1);
-					userPlayList.save();
-					await message.reply(`Creada la lista con el nombre: '${aux1}'`);
-					// Log aki
-				}
-			}
-			break;
-		case "remove":
-			if(!userPlayList || userPlayList.size == 0)
-				await message.reply("No hay listas de reproducción");
-			else if(args.length <= 1 || !isInteger(args[1]))
-				await message.reply("Falta el número de la lista");
-			else {
-				aux1 = parseInt(args[1]);
-				if(aux1 < 1 || aux1 > userPlayList.size)
-					await message.reply(`El índice debe estár entre 1 y ${userPlayList.size}`);
-				else {
-					
-				}
-			}
-			break;
-	}
 }
-
-/*
-
-try {
-	await command.execute(interaction);
-} catch (error) {
-	console.error(error);
-	if (interaction.replied || interaction.deferred) {
-		await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-	} else {
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-}
-*/
-
-/*
-Acciones a poder realizar por el usuario sobre su conjunto de playlists:
-- Crear playlist
- * Con nombre
- * Con nombre y descripción
-- Eliminar playlist
-- Editar playlist
-- Mostrar todas las playlist
-- Añadir las canciones de la playlist a la cola
-- Añadir una canción de la playlist a la cola
-
-Acciones sobre una playlist concreta:
-- Mostrar info
-- Añadir canción
-- Eliminar canción
-*/

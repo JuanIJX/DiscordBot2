ef10797bfe832877f98ab60f8c1ccd7ad11ff6ee
JuanIJX <srwe-@hotmail.com>
2023-06-04 00:18:57 +0200

    Edit test

--- src/commands.js
+++ src/commands.js
@@ -1,19 +1,33 @@
-import { Events } from "discord.js";
 import { isInteger, wait } from "./libraries/utils.mjs";
 import { Level } from "./libraries/logger.js";
+import { CRol, RolPerms, Type } from "./plugins/Autocanales/Canal.js";
 
 //function log(level, msg) { this._logger.log(level, this.name, msg); }
 
 export default async function(cadena, cmdName, args) {
-	var aux_1;
+	let user, member, guild;
+	let aux_1, aux_2, aux_3;
+
 	switch (cmdName) {
 		case "t":
 		case "test":
-			console.log(Object.values(Events));
+			aux_1 = this.modules.get("Autocanales").gestorCanales;
+			aux_2 = aux_1.list.get("772607985104060417"); // guildCanal
+			member = await aux_2.guild.members.fetch("171058039065935872").then(member => member);
+			user = member.user;
+
+			//await aux_2.createCanal(user);
+			aux_3 = aux_2.list.get("1114027592056582195"); // Canal
+			await aux_3.addBanned("754407873567654048");
+
+			console.log(aux_3.toJSON());
+
+			
+			//aux_3.channel.permissionOverwrites.edit(userID, perms);
 			break;
 		case "pl":
 		case "plugins":
-			var aux_1 = 0;
+			aux_1 = 0;
 			console.log(`Lista de plugins:`);
 			this.modules.forEach((value, key) => console.log(`- ${++aux_1} ${key}: ${value.started ? "ON" : "OFF"}`));
 			break;

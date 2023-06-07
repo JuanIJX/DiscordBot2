3bbadf5a11162f75b67f8cb255b1ebe8dd94daef
JuanIJX <srwe-@hotmail.com>
2023-06-08 01:36:22 +0200

    Repair test

--- src/inload.js
+++ src/inload.js
@@ -5,8 +5,8 @@ export default async function() {
 		const client = this.discordManager.discord;
 		const gestorCanales = this.modules.get("Autocanales").gestorCanales;
 
-		const guild = client.guilds.cache.get("772607985104060417");
-		const channel = guild.channels.cache.get("1114027592056582195");
+		//const guild = client.guilds.cache.get("772607985104060417");
+		//const channel = guild.channels.cache.get("1114027592056582195");
 		
 		//const guildCanal = gestorCanales.list.get("772607985104060417"); // guildCanal
 		//const canal = guildCanal.list.get("1115659179865153576");

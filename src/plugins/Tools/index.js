6b387a1662a4a02ee974d292e48b6e8052a215e8
JuanIJX <srwe-@hotmail.com>
2023-06-08 14:38:35 +0200

    Register command .di

--- src/plugins/Tools/index.js
+++ src/plugins/Tools/index.js
@@ -1,14 +1,14 @@
 import Module from "../../app/module.js"
 import funcDelete from "./commands/delete.js"
+import funcDeleteId from "./commands/deleteId.js"
 import funcMov from "./commands/mover.js"
-import { wait } from "../../libraries/utils.mjs";
 
 export default class Tools extends Module {
 	constructor() { super(...arguments); }
-
 	onLoad() {
 		this.color = 0x6600ff;
-		this.registerCommand(".d", funcDelete);
 		this.registerCommand(".m", funcMov);
+		this.registerCommand(".d", funcDelete);
+		this.registerCommand(".di", funcDeleteId);
 	}
 }

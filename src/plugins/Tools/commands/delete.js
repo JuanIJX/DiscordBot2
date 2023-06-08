5d0131fa7c26e23032e67df23a7fe3cdbc85a1eb
JuanIJX <srwe-@hotmail.com>
2023-06-08 14:37:38 +0200

    Improvement

--- src/plugins/Tools/commands/delete.js
+++ src/plugins/Tools/commands/delete.js
@@ -5,13 +5,14 @@ import { Level } from "../../../libraries/logger.js";
 export default async function(message, cmdName, args) {
 	let delay = 4000;
 	if(args.length <= 0)
-		message.tempReply(`Faltan parámetros, ejemplo: ${cmdName} <numero de lineas>`, delay);
+		await message.tempReply(`Faltan parámetros, ejemplo: ${cmdName} <numero de lineas>`, delay);
 	else if(!isInteger(args[0]) || parseInt(args[0]) <= 0)
-		message.tempReply(`El parámetro debe ser un número entero`, delay);
+		await message.tempReply(`El parámetro debe ser un número entero`, delay);
 	else if(!message.member.permissions.has(PermissionFlagsBits.Administrator))
-		message.tempReply(`No tienes permisos para ejecutar este comando`, delay);
-	else {
-		await message.channel.bulkDelete(parseInt(args[0])+1);
-		this.log(Level.HIST, `(g: ${message.guildId}) El usuario ${message.author.tag}(${message.author.id}) eliminó ${parseInt(args[0])} mensajes del canal ${message.channel.name}(${message.channel.id})`);
-	}
+		await message.tempReply(`No tienes permisos para ejecutar este comando`, delay);
+	else
+		await message.channel.bulkDelete(parseInt(args[0])+1)
+			.then(messages => this.log(Level.HIST, `(g: ${message.guildId}) El usuario ${message.author.tag}(${message.author.id}) eliminó ${messages.size} mensajes del canal ${message.channel.name}(${message.channel.id})`))
+			.catch(async error => await message.tempReply(error.message, delay)
+		);
 }

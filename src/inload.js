d7afb4b1a16690ee8fa4558336361c12bc1e7520
JuanIJX <srwe-@hotmail.com>
2023-06-08 15:13:59 +0200

    Delete code

--- src/inload.js
+++ src/inload.js
@@ -1,15 +1,6 @@
-import { PermissionFlagsBits, PermissionsBitField } from "discord.js";
-
 export default async function() {
 	try {
-		const client = this.discordManager.discord;
-		const gestorCanales = this.modules.get("Autocanales").gestorCanales;
-
-		//const guild = client.guilds.cache.get("772607985104060417");
-		//const channel = guild.channels.cache.get("1114027592056582195");
 		
-		//const guildCanal = gestorCanales.list.get("772607985104060417"); // guildCanal
-		//const canal = guildCanal.list.get("1115659179865153576");
 	} catch (error) {
 		console.log("ERROR DE IN LOAD");
 		console.log(error);

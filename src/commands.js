137671c4adbff6e66065513a1340a74c21fa2c97
JuanIJX <srwe-@hotmail.com>
2023-09-02 04:14:38 +0200

    Fix guildinfo

--- src/commands.js
+++ src/commands.js
@@ -125,11 +125,9 @@ export default async function(cadena, cmdName, args) {
 					aux_1 = await guild.roles.fetch();
 					aux_2 = {};
 					for (const [_, vs] of guild.voiceStates.cache) {
-						channel = await guild.channels.fetch(vs.channelId);
-						if(!channel) {
-							console.log(`ERROR EN CHANNEL ID(${vs.channelId})`)
+						if(!vs.channelId)
 							continue;
-						}
+						channel = await guild.channels.fetch(vs.channelId);
 						if(!aux_2.hasOwnProperty(channel.id))
 							aux_2[channel.id] = { channel, vs: [] };
 						aux_2[channel.id].vs.push(vs);
